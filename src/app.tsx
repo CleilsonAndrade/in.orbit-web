a866d19a8ef53757c5058d0c078775d7debc5967
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-09-15 20:57:56 -0300

    feat: screen empty goals

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,24 @@
+import letsStart from '@/assets/lets-start.svg'
+import logo from '@/assets/logo-in-orbit.svg'
+import { Plus } from 'lucide-react'
+
 export function App() {
   return (
-    <div>
-      <h1 className="text-zinc-50">teste</h1>
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="logo in-orbit" />
+      <img src={letsStart} alt="lets start illustration" />
+
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tighter hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
     </div>
   )
 }
