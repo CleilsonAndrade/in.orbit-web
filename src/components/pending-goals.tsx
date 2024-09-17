ba794397cb148dc7a1c2912537bda23c681e9c60
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-09-17 14:47:55 -0300

    feat(pending-goals): handle complete goal with refreshing component

--- src/components/pending-goals.tsx
+++ src/components/pending-goals.tsx
@@ -1,9 +1,12 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { Plus } from 'lucide-react'
+import { createGoalCompletion } from '../http/create-goal-completion'
 import { getPendingGoals } from '../http/get-pending-goals'
 import { OutlineButton } from './ui/outline-button'
 
 export function PendingGoals() {
+  const queryClient = useQueryClient()
+
   const { data } = useQuery({
     queryKey: ['pending-goals'],
     queryFn: getPendingGoals,
@@ -13,6 +16,13 @@ export function PendingGoals() {
     return null
   }
 
+  async function handleCompleteGoal(goalId: string) {
+    await createGoalCompletion(goalId)
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+  }
+
   return (
     <div className="flex flex-wrap gap-3">
       {data.pendingGoals.map(goal => {
@@ -20,6 +30,7 @@ export function PendingGoals() {
           <OutlineButton
             key={goal.id}
             disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleCompleteGoal(goal.id)}
           >
             <Plus className="size-4 text-zinc-600" />
             {goal.title}
---
+++ src/http/create-goal-completion.ts
@@ -0,0 +1,11 @@
+export async function createGoalCompletion(goalId: string) {
+  await fetch('', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      goalId,
+    }),
+  })
+}
