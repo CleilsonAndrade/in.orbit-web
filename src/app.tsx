dcbc3ac7ad9f01969cbff81babdffb67868bead6
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-09-17 13:29:43 -0300

    feat(fetch): fecth data to summary with react-query and setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
 import { CreateGoal } from './components/create-goal'
+import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
-// import { EmptyGoals } from './components/empty-goals'
 import { Dialog } from './components/ui/dialog'
+import { getSummary } from './http/get-summary'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
   )
 }
-
-export default App
---
+++ src/http/get-summary.ts
@@ -0,0 +1,18 @@
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
+
+export async function getSummary(): Promise<SummaryResponse> {
+  const response = await fetch('')
+  const data = await response.json()
+  return data.summary
+}
